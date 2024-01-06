476cb3b786a31f344741fc457b0d0f2b1b69ef5b
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-01-06 20:28:25 +0530

    Add initial files

---
+++ swiggy.js
@@ -0,0 +1,8 @@
+// setInterval(() => {
+//   document.getElementById("moveoffer").scrollBy(300,0)
+//   });
+// }, 500);
+
+setInterval(() => {
+  document.getElementById("moveoffer").scrollBy(300, 0);
+}, 2000);
